07e4e146472c154aca08ca8b1a16142fcf134dc2
Ron Dahlgren <ronald.dahlgren@gmail.com>
2013-06-09 17:59:33 -0700

    The thumbnail example., Rust-ified!

---
+++ samples/thumbnail.rs
@@ -0,0 +1,86 @@
+/*!
+ * The first code example from http://www.imagemagick.org/script/magick-wand.php
+ * 
+ * This program reads a image, creates a thumbnail, and writes out the result.
+ * Usage:
+ *    ./thumbnail inputfile [-o outputfile]
+ */
+extern mod extra;
+extern mod rustwand;
+
+use extra::getopts::*;
+use rustwand::*;
+use std::os;
+
+fn print_usage(name: &str, _opts: &[Opt]) {
+  println(fmt!("Usage: %s input_file [options]", name));
+  println("-o\t\toutput_file");
+  println("-h --help\tUsage");
+}
+
+fn main() {
+  let args = os::args();
+  let program_name = copy args[0];
+  let opts = ~[ optopt("o"), optflag("h"), optflag("help") ];
+  let matches = match getopts(args.tail(), opts) {
+    Ok(m) => { m }
+    Err(f) => { fail!(fail_str(f)) }
+  };
+  // Detect a help request
+  if opt_present(&matches, "h") || opt_present(&matches, "help") {
+    print_usage(program_name, opts);
+    return;
+  }
+  let output = opt_maybe_str(&matches, "o");
+  // Ensure that at least the input file is specified
+  let input: &str = if !matches.free.is_empty() { 
+    copy matches.free[0]
+  } else {
+    print_usage(program_name, opts);
+    return;
+  };
+  thumbnail_it(input, output);
+}
+
+fn generate_default_output_filename(input: &str) -> ~str {
+  // see if the filename has a period in it. If so, I assume that
+  // the last occurance of the period denotes the file extension
+  let append_index = match std::str::rfind_char(input, '.') {
+    Some(index) => index,
+    None => input.len()
+  };
+  // TODO: Figure out wtf with these boxes
+  std::str::concat_slices(&[
+    std::str::slice(input, 0, append_index),
+    &"-thumbnail.png"
+  ])
+}
+
+fn thumbnail_it(in: &str, out: Option<~str>) {
+  let out_file = match out {
+   None => generate_default_output_filename(in),
+   Some(filename) => filename
+  };
+
+  println(fmt!("Will be reading from %s", in));
+  println(fmt!("Will be writing to %s", out_file));
+
+  unsafe { MagickWandGenesis();}
+  let wand = unsafe { NewMagickWand() };
+  in.as_c_str(|buffer| {
+    unsafe { MagickReadImage(wand, buffer); }
+  });
+  unsafe { MagickResetIterator(wand); }
+  unsafe {
+    while (MagickNextImage(wand) != MagickFalse) {
+     MagickResizeImage(wand, 106, 80, LanczosFilter, 1.0); 
+    }
+  }
+  out_file.as_c_str(|buffer| {
+    unsafe { MagickWriteImages(wand, buffer, MagickTrue); }
+  });
+
+  unsafe { DestroyMagickWand(wand); }
+
+  unsafe { MagickWandTerminus(); }
+}
